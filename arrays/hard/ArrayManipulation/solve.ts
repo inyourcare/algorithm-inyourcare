39990db1a3995b3bc2f88db5f9d217f458273ecd
Kwon KiHoon <inyourcareallen@gmail.com>
2024-04-05 19:46:16 +0900

    trial3

--- arrays/hard/ArrayManipulation/solve.ts
+++ arrays/hard/ArrayManipulation/solve.ts
@@ -32,25 +32,75 @@ function readLine(): string {
  *  2. 2D_INTEGER_ARRAY queries
  */
 
+// function arrayManipulation(n: number, queries: number[][]): number {
+//   // Write your code here
+//   // console.log('given n is' , n)
+//   const arr = new Array(n).fill(0);
+//   let max = 0
+//   // console.log('arr created ',arr)
+//   for (let i = 0; i < queries.length; i++) {
+//     const [start,end,val] = queries[i];
+//     // console.log(start,end,val)
+//     for (let j = start-1; j < end; j++) {
+//       arr[j]+=val;
+//       if (arr[j]>max)
+//         max = arr[j]
+//     }
+//     // console.log(i,'operaction done, arr is',arr)
+//   }
+//   // return Math.max(...arr);
+//   return max;
+// }
+
 function arrayManipulation(n: number, queries: number[][]): number {
-  // Write your code here
-  // console.log('given n is' , n)
-  const arr = new Array(n).fill(0);
-  let max = 0
-  // console.log('arr created ',arr)
+  const indicesValArr = [[1, n, 0]];
+  let max = 0;
   for (let i = 0; i < queries.length; i++) {
-    const [start,end,val] = queries[i];
-    // console.log(start,end,val)
-    for (let j = start-1; j < end; j++) {
-      arr[j]+=val;
-      if (arr[j]>max)
-        max = arr[j]
+    const [qsi, qei, qVal] = queries[i];
+    // console.log("indicesValArr is", indicesValArr);
+    // console.log("query is", queries[i]);
+    let j = 0;
+    // for (let j = 0; j < indiceValArr.length; j++) {
+    while (j < indicesValArr.length) {
+      const [dsi, dei, dVal] = indicesValArr[j];
+      let tmpVal = 0;
+      if ((qsi === dsi && qei === dei) || (qsi <= dsi && qei >= dei)) {
+        tmpVal = indicesValArr[j][2] + qVal;
+        indicesValArr[j][2] = tmpVal;
+      } else if (!((qsi < dsi && qei < dsi) || (qsi > dei && qei > dei))) {
+        // const orderedIndice = [qsi, dsi, dei, qei].sort().reverse();
+        const orderedIndices = [qsi, dsi, dei, qei].filter((e, index) => {
+          return e <= dei && e >= dsi;
+        }).sort((a, b) => a - b);
+        // orderedIndices.sort((a, b) => a - b);
+        // console.log("orderedIndices is", orderedIndices);
+        const tmpArr = new Array<number[]>();
+        for (let k = 0; k < orderedIndices.length - 1; k++) {
+          let start = orderedIndices[k];
+          let end = orderedIndices[k + 1];
+          if (start === qei && end > qei) start += 1;
+          if (end === qsi && start < qsi) end -= 1;
+          const tmp = dVal + (start >= qsi && end <= qei ? qVal : 0);
+          tmpArr.push([start, end, tmp]);
+          if (tmp > tmpVal) tmpVal = tmp;
+        }
+        indicesValArr.splice(j, 1, ...tmpArr);
+        j += tmpArr.length - 1;
+      }
+      // console.log(j, indicesValArr);
+      if (max < tmpVal) max = tmpVal;
+      j++;
     }
-    // console.log(i,'operaction done, arr is',arr)
   }
-  // return Math.max(...arr);
   return max;
 }
+// function arrayManipulation(n: number, queries: number[][]): number {
+//   // index 경우의 수
+//   // qsi < dsi , qei < dei / qsi < dsi , qei > dei / qsi < dsi , qei = dei
+//   // qsi > dsi , qei < dei / qsi > dsi , qei > dei / qsi > dsi , qei = dei
+//   // qsi = dsi , qei < dei / qsi = dsi , qei > dei / qsi = dsi , qei = dei
+//   return 0
+// }
 
 function main() {
   const ws: WriteStream = createWriteStream("output.txt");
